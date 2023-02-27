f7dcba04814a01bc16bdd5bca3af1de145e99787
Aniket Vardey <63118235+AniketVardey@users.noreply.github.com>
2023-02-27 15:43:31 +0530

    Update and rename lineage_G.mk to superior_G.mk

--- superior_G.mk
+++ superior_G.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/10or/G/device.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
